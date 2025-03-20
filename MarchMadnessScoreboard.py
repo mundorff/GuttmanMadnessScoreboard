7cab2ac11a5ccaa1e66ae0ba9221165734fc4c8b
mundorff <60701188+mundorff@users.noreply.github.com>
2025-03-20 10:18:10 -0700

    Update MarchMadnessScoreboard.py

--- MarchMadnessScoreboard.py
+++ MarchMadnessScoreboard.py
@@ -174,7 +174,7 @@ def update_scores():
             potential_remaining += potential_points
             
             if team in losers:
-                teams_with_seeds.append(f'<span style="color: red; font-weight: bold;">(L)</span>{team} ({seed})')
+                teams_with_seeds.append(f'(L){team} ({seed})')
             else:
                 teams_with_seeds.append(f"{team} ({seed})")
 
